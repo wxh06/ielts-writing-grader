d6abd19876968df9c579094590e767c70a42b91b
汪心禾 <wangxinhe06@gmail.com>
2023-08-04 16:21:51 +0800

    Fetch more samples from writing9

--- crawler/crawler/spiders/writing9_spider.py
+++ crawler/crawler/spiders/writing9_spider.py
@@ -7,19 +7,20 @@ from scrapy.linkextractors import LinkExtractor
 class Writing9Spider(scrapy.Spider):
     name = "writing9"
     start_urls = ("https://writing9.com/ielts-writing-samples",)
+    slugs = []
 
     def parse(self, response):
         for link in LinkExtractor(
-            allow=r"https:\/\/writing9\.com\/text\/"
+            allow=r"^https:\/\/writing9\.com\/text\/"
         ).extract_links(response):
-            yield scrapy.Request(url=link.url, callback=self.parse_sample)
+            yield scrapy.Request(url=link.url, callback=self.parse)
 
-    def parse_sample(self, response):
-        props = json.loads(response.css("#__NEXT_DATA__::text").get())["props"][
-            "pageProps"
-        ]
-        yield {
-            "question": props["text"]["question"].replace("\r\n", "\n"),
-            "essay": props["text"]["text"].replace("\r\n", "\n"),
-            "band": props["results"]["bands"]["band"],
-        }
+        try:
+            props = json.loads(response.css("#__NEXT_DATA__::text").get())["props"][
+                "pageProps"
+            ]
+            if props["id"] not in self.slugs:
+                self.slugs.append(props["id"])
+                yield props
+        except KeyError:
+            pass
